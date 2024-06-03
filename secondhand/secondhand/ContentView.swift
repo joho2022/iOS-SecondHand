754761be5667c97fa330a5513256143ee4e14378
joho2022 <josama1997@naver.com>
2024-06-03 21:41:04 +0900

    💄 Feat(#1): 로그인 화면 UI

--- secondhand/secondhand/ContentView.swift
+++ secondhand/secondhand/ContentView.swift
@@ -8,17 +8,107 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("홈화면")
+                        .font(.system(.regular, size: 17))
+                }
+            SalesHistoryView()
+                .tabItem {
+                    Image(systemName: "doc.plaintext")
+                    Text("판매내역")
+                }
+            FavoritesView()
+                .tabItem {
+                    Image(systemName: "heart")
+                    Text("관심목록")
+                }
+            ChatView()
+                .tabItem {
+                    Image(systemName: "message")
+                    Text("채팅")
+                }
+            ProfileView()
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("내 계정")
+                }
         }
-        .padding()
+        .tint(.orange)
+    }
+}
+
+struct ProfileView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                HStack(spacing: 30) {
+                    Text("아이디")
+                    TextField("아이디를 입력하세요", text: .constant(""))
+                        .multilineTextAlignment(.leading)
+                }
+                .padding(.horizontal)
+                .padding(.top, 90)
+                
+                Divider()
+                
+                Spacer()
+                
+                Button {
+                    print("로그인")
+                } label: {
+                    Text("로그인")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .foregroundColor(.customWhite)
+                        .background(.customOrange)
+                        .cornerRadius(8)
+                        .font(.system(.regular, size: 15))
+                }
+                .padding()
+                
+                Button {
+                    print("회원가입")
+                } label: {
+                    Text("회원가입")
+                        .font(.system(.regular, size: 15))
+                        .foregroundColor(.customGray900)
+                }
+                .padding(.bottom, 90)
+            }
+            .navigationBarTitle("내 계정", displayMode: .inline)
+        }
+    }
+}
+
+struct HomeView: View {
+    var body: some View {
+        Text("홈화면")
+    }
+}
+
+struct SalesHistoryView: View {
+    var body: some View {
+        Text("판매내역")
+    }
+}
+
+struct FavoritesView: View {
+    var body: some View {
+        Text("관심목록")
+    }
+}
+
+struct ChatView: View {
+    var body: some View {
+        Text("채팅")
     }
 }
 
 #Preview {
-    ContentView()
+    ProfileView()
 }
