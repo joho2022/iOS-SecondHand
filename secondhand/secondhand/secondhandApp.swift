ec0221ecd3dcb5f6141e5e620d8e2382994b3313
joho2022 <josama1997@naver.com>
2024-06-03 16:31:28 +0900

    ✨ Feat(#1): 프로젝트 생성

---
+++ secondhand/secondhand/secondhandApp.swift
@@ -0,0 +1,17 @@
+//
+//  secondhandApp.swift
+//  secondhand
+//
+//  Created by 조호근 on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct secondhandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
