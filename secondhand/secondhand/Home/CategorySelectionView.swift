6e2ab57d966ef7908b4eedf621509f80e20096ee
joho2022 <josama1997@naver.com>
2024-06-19 12:46:15 +0900

    💄 Feat(#5): 아이콘 svg파일 추가

---
+++ secondhand/secondhand/Home/CategorySelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  CategorySelectionView.swift
+//  secondhand
+//
+//  Created by 조호근 on 6/18/24.
+//
+
+import SwiftUI
+
+struct CategorySelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategorySelectionView()
+}
