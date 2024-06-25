53b77e7befc9a2453b15f8e39ff092ce99e27b21
joho2022 <josama1997@naver.com>
2024-06-25 15:33:42 +0900

    ♻️ Refactor(#12): 테스트코드에 Mock데이터 생성으로 개선

--- secondhand/secondhandTests/ProductListViewModelTests.swift
+++ secondhand/secondhandTests/ProductListViewModelTests.swift
@@ -22,15 +22,12 @@ class MockProductManager: ProductManagerProtocol {
     var productsPublisher: Published<[Product]>.Publisher { $products }
     
     func loadMockProducts() {
-        guard let url = Bundle.main.url(forResource: "Products", withExtension: "json") else {
-            XCTFail("Missing file: Products.json")
-            return
-        }
+        let mockProducts: [Product] = [
+            Product(id: 0, title: "나이키 티셔츠", price: 40000, location: "역삼동", category: [.popular], image: "https://kream-phinf.pstatic.net/MjAyMzA0MTRfMjAw/MDAxNjgxNDUxNDAyMTUz.5q0cKoTNu0T3fLSHXHRomwuYI5EE3bxDXCcjHFeZnuUg.ts4o7ZUeK19uolxTSWNmVNDCr_mg9472IQ0YZcj0heIg.JPEG/a_d7263afeb4e04111abafbda2b5b67bea.jpg?type=l_webp", timePosted: "2024-06-10T13:14:08", likes: 10, comments: 2, isReserved: false, user: "100", description: "멋진 나이키 티셔츠입니다."),
+            Product(id: 1, title: "조던 신발", price: 500000, location: "역삼동", category: [.popular], image: "https://kream-phinf.pstatic.net/MjAyNDA1MjRfMzkg/MDAxNzE2NTMyODQzNTgy.ECw3Xikv8w6R1-zs-OcQyVY-SMAN1tSInutYzMkUmNkg.cI4OQfPajMr34SNzLX32WaNQopL02vNol6_yWMQyl8gg.PNG/a_0a36d3746db04595871a75bcc6798e02.png?type=l_webp", timePosted: "2024-06-10T13:14:08", likes: 25, comments: 5, isReserved: false, user: "101", description: "새로운 조던 신발입니다.")
+        ]
         
-        let data = try! Data(contentsOf: url)
-        let products = try! JSONDecoder().decode([Product].self, from: data)
-        
-        self.products = products
+        self.products = mockProducts
     }
     
     func getProducts() -> [Product] {
@@ -73,19 +70,23 @@ final class ProductListViewModelTests: XCTestCase {
         
         productManager.loadMockProducts()
         
-        productManager.$products
-            .sink { products in
-                XCTAssertFalse(products.isEmpty, "Mock 데이터가 있어야 한다.")
+        viewModel.$filteredProducts
+            .dropFirst()
+            .sink { filteredProducts in
+                XCTAssertFalse(filteredProducts.isEmpty, "Mock 데이터가 있어야 한다.")
                 
-                XCTAssertEqual(self.viewModel.filteredProducts.count, products.count, "로드된 데이터 수가 일치해야 한다.")
+                let mockProducts = self.productManager.getProducts()
+                XCTAssertEqual(filteredProducts.count, mockProducts.count, "로드된 데이터 수가 일치해야 한다.")
                 
-                for (index, product) in products.enumerated() {
-                    XCTAssertEqual(self.viewModel.filteredProducts[index].title, product.title, "제품 이름이 일치해야 합니다.")
+                for (index, product) in mockProducts.enumerated() {
+                    XCTAssertEqual(filteredProducts[index].title, product.title, "제품 이름이 일치해야 합니다.")
                 }
                 
                 expectation.fulfill()
             }
             .store(in: &cancellables)
+        
+        viewModel.filterProducts()
         
         wait(for: [expectation], timeout: 1.0)
     }
